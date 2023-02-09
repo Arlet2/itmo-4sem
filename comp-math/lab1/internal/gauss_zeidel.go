f624182968fe389c13648497a899bff573108478
korlir <anonym.show@mail.ru>
2023-02-09 23:39:45 +0300

    Add Gauss and Zeidel method

--- comp-math/lab1/internal/gauss_zeidel.go
+++ comp-math/lab1/internal/gauss_zeidel.go
@@ -6,10 +6,10 @@ import (
 )
 
 /*
-	1. Ищем максимальный элемент в строке, записываем индекс
-	2. Проверяем по условию в каждой строке (сумма остальных элементов меньше максимального)
-	3. Добавляем максимальный индекс каждого элемента в коллекцию
-	4. Проверяем: если индексы различны, то возможно перестроить матрицу так, чтобы она обладала диагональным преобладанием, иначе нет
+1. Ищем максимальный элемент в строке, записываем индекс
+2. Проверяем по условию в каждой строке (сумма остальных элементов меньше максимального)
+3. Добавляем максимальный индекс каждого элемента в коллекцию
+4. Проверяем: если индексы различны, то возможно перестроить матрицу так, чтобы она обладала диагональным преобладанием, иначе нет
 */
 func (m *matrix) TryToCreateDiagonalDominance() bool {
 
@@ -20,11 +20,11 @@ func (m *matrix) TryToCreateDiagonalDominance() bool {
 
 	maxIndexes := map[int][]float64{}
 
-	for i:=0; i<m.size; i++ {
+	for i := 0; i < m.size; i++ {
 		maxCoeff = math.Abs(m.coeff[i][0])
 		maxIndex = 0
 		// поиск максимального элемента
-		for j := 0; j<m.size;j++ {
+		for j := 0; j < m.size; j++ {
 			if maxCoeff < math.Abs(m.coeff[i][j]) {
 				maxIndex = j
 				maxCoeff = math.Abs(m.coeff[i][j])
@@ -32,7 +32,7 @@ func (m *matrix) TryToCreateDiagonalDominance() bool {
 		}
 		rowSum = 0
 		// проверка условия
-		for j := 0; j<m.size; j++ {
+		for j := 0; j < m.size; j++ {
 			// игнорируем значение максимального элемента
 			if j == maxIndex {
 				continue
@@ -57,10 +57,6 @@ func (m *matrix) TryToCreateDiagonalDominance() bool {
 		maxIndexes[maxIndex] = m.coeff[i]
 	}
 
-	for key, val := range maxIndexes {
-		fmt.Printf("[%d] = %f\n", key, val)
-	}
-
 	if len(maxIndexes) != m.size {
 		return false
 	}
@@ -70,10 +66,86 @@ func (m *matrix) TryToCreateDiagonalDominance() bool {
 		return false
 	}
 
-	for i:=0;i<m.size;i++ {
+	for i := 0; i < m.size; i++ {
 		m.coeff[i] = maxIndexes[i]
 	}
 
 	return true
 }
 
+func (m matrix) UseGaussZeidel(eps float64, withTrace bool) {
+
+	var specialMatrix matrix
+	coeff := make([][]float64, m.size)
+	// приводим матрицу к виду x = Cx + d
+	for index := range m.coeff {
+		m.MulRow(index, 1/m.coeff[index][index])
+
+		for j := 0; j < m.size; j++ {
+			if index == j {
+				continue
+			}
+			m.coeff[index][j] *= -1
+		}
+		coeff[index] = []float64{}
+
+		for jndex, value := range m.coeff[index] {
+			if jndex == index {
+				continue
+			}
+			coeff[index] = append(coeff[index], value)
+		}
+	}
+
+	specialMatrix = matrix{size: m.size, coeff: coeff}
+
+	fmt.Println("\nМатрица вида x = Cx+d:")
+	specialMatrix.Print()
+
+	counter := 0
+	diff := m.coeff[0][0]
+	var last float64
+	var shift int
+
+	d := make([]float64, m.size)
+	faults := make([]float64, m.size)
+
+	// задаём начальное приближение
+	for index := range specialMatrix.coeff {
+		d[index] = specialMatrix.coeff[index][specialMatrix.size-1]
+	}
+
+	for diff >= eps {
+		if (withTrace) {
+			fmt.Println(d, "diff:",diff)
+		}
+		counter++
+		diff = 0
+
+		for i := 0; i < specialMatrix.size; i++ {
+			last = d[i]
+			d[i] = 0
+			shift = 0
+			for j := 0; j < m.size; j++ {
+				if j == i {
+					shift = 1
+				}
+
+				if j+1 == m.size {
+					d[i] += specialMatrix.coeff[i][j]
+					continue
+				}
+
+				d[i] += specialMatrix.coeff[i][j] * d[j+shift]
+			}
+			faults[i] = math.Abs(d[i]-last)
+			diff = math.Max(diff, faults[i])
+
+		}
+
+	}
+	fmt.Println("\nВыполнено за", counter, "итерации(-й).")
+	fmt.Println("Вектор неизвестных:", d)
+	fmt.Println("Вектор погрешностей:", faults)
+
+}
