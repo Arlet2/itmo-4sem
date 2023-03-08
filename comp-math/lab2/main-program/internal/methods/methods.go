d2f0fea5b5893e61dafc7a47e909d7e469c8bf4d
korlir <anonym.show@mail.ru>
2023-03-09 01:46:38 +0300

    Add root checking

--- comp-math/lab2/main-program/internal/methods/methods.go
+++ comp-math/lab2/main-program/internal/methods/methods.go
@@ -21,3 +21,11 @@ type MethodInfo struct {
 	Name   string
 	Action func(functions.Function) (root float64, err error)
 }
+
+func HasIntervalRoot(function functions.Function, a int, b int) bool {
+	return HasIntervalRoots(function, a, b) && function.Derivative(float64(a))*function.Derivative(float64(b)) > 0
+}
+
+func HasIntervalRoots(function functions.Function, a int, b int) bool {
+	return a*b < 0
+}
