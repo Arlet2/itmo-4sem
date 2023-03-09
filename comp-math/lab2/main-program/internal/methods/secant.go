8a018707d2cd5047028dafcf7e356901ff550cbb
korlir <anonym.show@mail.ru>
2023-03-09 23:46:12 +0300

    Add secant method

--- comp-math/lab2/main-program/internal/methods/secant.go
+++ comp-math/lab2/main-program/internal/methods/secant.go
@@ -3,8 +3,14 @@ package methods
 import (
 	"lab2/internal/functions"
 	"lab2/internal/readers"
+	"math"
 )
 
 func secantMethod(function functions.Function, readInfo readers.ReadInfo) (root float64, err error) {
+	previousRoot := readInfo.Approx[0]
+	root = readInfo.Approx[1]
+	for math.Abs(previousRoot-root) > readInfo.Eps && math.Abs(function.Formula(root)) > readInfo.Eps {
+		root = root - (root-previousRoot)*function.Formula(root)/(function.Formula(root)-function.Formula(previousRoot))
+	}
 	return
 }
