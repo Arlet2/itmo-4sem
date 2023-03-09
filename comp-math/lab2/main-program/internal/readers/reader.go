e3af8e03e8fe8378e88305374f6330352530c649
Arlet2 <shulga@niuitmo.ru>
2023-03-09 12:48:42 +0300

    Add reading by file

--- comp-math/lab2/main-program/internal/readers/reader.go
+++ comp-math/lab2/main-program/internal/readers/reader.go
@@ -109,3 +109,95 @@ func ReadByConsole(function functions.Function, method methods.MethodInfo) (read
 
 	return
 }
+
+func ReadByFile(path string, function functions.Function, method methods.MethodInfo) (readInfo ReadInfo, err error) {
+
+	file, err := os.OpenFile(path, os.O_RDONLY, os.ModeDevice)
+
+	if err != nil {
+		err = errors.Join(err, errors.New("Не удалось открыть файл"))
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	if !scanner.Scan() {
+		err = errors.New("ожидался ввод в первой строке")
+		return
+	}
+
+	readInfo.LeftBorder, err = strconv.ParseFloat(scanner.Text(), 64)
+
+	if err != nil {
+		err = errors.New("ожидалось целое число в первой строке")
+		return
+	}
+
+	if !scanner.Scan() {
+		err = errors.New("ожидался ввод во второй строке")
+		return
+	}
+
+	readInfo.RightBorder, err = strconv.ParseFloat(scanner.Text(), 64)
+
+	if err != nil {
+		err = errors.New("ожидалось целое число во второй строке")
+		return
+	}
+
+	if readInfo.LeftBorder > readInfo.RightBorder {
+		readInfo.LeftBorder, readInfo.RightBorder = readInfo.RightBorder, readInfo.LeftBorder
+	}
+
+	if !scanner.Scan() {
+		err = errors.New("ожидался ввод в третьей строке")
+		return
+	}
+
+	readInfo.Eps, err = strconv.ParseFloat(scanner.Text(), 64)
+
+	if err != nil {
+		err = errors.New("ожидалось число в третьей строке")
+		return
+	}
+
+	switch method.Id {
+	case 1:
+		readInfo.Approx = make([]float64, 2)
+
+		var isInputed bool
+
+		if scanner.Scan() {
+			readInfo.Approx[0], err = strconv.ParseFloat(scanner.Text(), 64)
+
+			if err == nil {
+				isInputed = true
+			}
+		}
+
+		if !isInputed {
+			fmt.Println("Было использовано значение по умолчанию для первого приближения")
+			readInfo.Approx[0] = methods.GetFirstApprox(function, readInfo.LeftBorder, readInfo.RightBorder)
+		}
+
+		isInputed = false
+
+		if scanner.Scan() {
+			readInfo.Approx[1], err = strconv.ParseFloat(scanner.Text(), 64)
+
+			if err == nil {
+				isInputed = true
+			}
+		}
+
+		if !isInputed {
+			fmt.Println("Было использовано значение по умолчанию для второго приближения")
+			readInfo.Approx[1] = methods.GetSecondApprox(readInfo.Approx[0], readInfo.LeftBorder, readInfo.RightBorder)
+		}
+
+		err = nil
+		
+	}
+	
+	return
+}
