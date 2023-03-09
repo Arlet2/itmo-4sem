64de18a54ecf5fac6c45a71fe1ce2311bbcff7c1
Arlet2 <shulga@niuitmo.ru>
2023-03-09 23:33:55 +0300

    Add prototype of main

--- comp-math/lab2/main-program/cmd/main.go
+++ comp-math/lab2/main-program/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"lab2/internal/functions"
 	"lab2/internal/methods"
+	"lab2/internal/readers"
 	"os"
 	"strconv"
 )
@@ -29,12 +30,14 @@ func main() {
 		return
 	}
 
-	if int(input-1) < 0 || int(input) >= len(functions.Functions) {
+	if int(input-1) < 0 || int(input) > len(functions.Functions) {
 		fmt.Printf("Ожидалось число в промежутке от 1 до %d\n", len(functions.Functions))
 		return
 	}
 
-	fmt.Println("Вы выбрали функцию f(x) = " + functions.Functions[int(input-1)].Text)
+	function := functions.Functions[int(input-1)]
+
+	fmt.Println("Вы выбрали функцию f(x) = " + function.Text)
 
 	fmt.Println("Выберите метод:")
 
@@ -54,13 +57,67 @@ func main() {
 		return
 	}
 
-	if int(input-1) < 0 || int(input) >= len(methods.Methods) {
+	if int(input-1) < 0 || int(input-1) >= len(methods.Methods) {
 		fmt.Printf("Ожидалось число в промежутке от 1 до %d\n", len(methods.Methods))
 		return
 	}
 
-	fmt.Println("Вы выбрали " + methods.Methods[input-1].Name)
+	method := methods.Methods[input-1]
+	fmt.Println("Вы выбрали " + method.Name)
 
-	// добавить ввод из файла
+	fmt.Println("Выберите: 1) ввод из файла; 2) ввод из консоли; 3) ввод из lab2.txt")
+
+	if !scanner.Scan() {
+		fmt.Println("Ожидался ввод...")
+		return
+	}
+
+	input, err = strconv.ParseInt(scanner.Text(), 10, 64)
+
+	if err != nil {
+		fmt.Println("Ожидалось целое число")
+		return
+	}
+
+	var readInfo readers.ReadInfo
+
+	if input == 1 {
+		fmt.Print("Введите путь до файла: ")
+		if !scanner.Scan() {
+			fmt.Println("Ожидался ввод...")
+			return
+		}
+
+		readInfo, err = readers.ReadByFile(scanner.Text(), function, method.Id)
+	} else if input == 2 {
+		readInfo, err = readers.ReadByConsole(function, method.Id)
+	} else if input == 3 {
+		readInfo, err = readers.ReadByFile("lab3.txt", function, method.Id)
+	} else {
+		fmt.Println("Ожидалось число от 1 до 3")
+		return
+	}
+
+	if err != nil {
+		fmt.Println("Ошибка: " + err.Error())
+		return
+	}
+
+	fmt.Println(readInfo)
+
+	if !functions.HasIntervalRoots(function, readInfo.LeftBorder, readInfo.RightBorder) {
+		fmt.Println("ВНИМАНИЕ! На данном интервале ВОЗМОЖНО нет корней. Результаты могут быть некорректны")
+	} else if !functions.HasIntervalRoot(function, readInfo.LeftBorder, readInfo.RightBorder) {
+		fmt.Println("ВНИМАНИЕ! На данном интервале НЕСКОЛЬКО корней. Будет найден произвольный корень")
+	}
+
+	root, err := method.Action(function, readInfo)
+
+	if err != nil {
+		fmt.Println("Ошибка: " + err.Error())
+		return
+	}
+
+	fmt.Printf("Найденный корень равен: %f\n", root)
 
 }
